18f390f8f5cc9a1d59f869e9469773ef0f5b90fc
kli44mar <71692931+kli44mar@users.noreply.github.com>
2022-04-19 21:03:21 +0500

    gui update

--- gui.py
+++ gui.py
@@ -2,12 +2,10 @@ import sys
 import threading
 import IIPlay
 
-from multiprocessing.connection import wait
-from IPython.external.qt_for_kernel import QtCore
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtGui import QPen, QPainter, QBrush
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QDialog, QDialogButtonBox, \
-    QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QDialog, QDialogButtonBox, QVBoxLayout, \
+    QMessageBox
 
 from Board import Board
 
@@ -24,7 +22,8 @@ class MainWindow(QMainWindow):
         self.width_desk = int(self.height_desk * 0.9)
 
         self.setFixedSize(QSize(self.width_desk, self.height_desk))
-        self.button.clicked.connect(self.open_dialog)
+        self.count_pass = 0
+        self.button.clicked.connect(self.add_count_pass)
         self.button.setGeometry(int(self.width_desk * 0.4), int(self.height_desk * 0.9), int(self.width_desk * 0.2),
                                 int(self.height_desk * 0.05))
         self.indent = int(self.width_desk * 0.1)
@@ -32,10 +31,16 @@ class MainWindow(QMainWindow):
         self.board = Board()
         self.person_point = None
         self.click_pass = False
-        self.count_pass = 0
         self.flag = True
+        self.dlg = QMessageBox(self)
+
+    def add_count_pass(self):
+        """sums up the number of passes"""
+        self.count_pass += 1
+        self.click_pass = True
 
     def paintEvent(self, e):
+        """draws a board and stones"""
         qp = QPainter()
         qp.begin(self)
         self.draw_board(qp)
@@ -43,18 +48,25 @@ class MainWindow(QMainWindow):
         qp.end()
 
     def mousePressEvent(self, a0):
+        """reacts to right-click and records coordinates"""
         self.person_point = (round((a0.x() - self.indent) / self.size_sq) + 1,
                              round((a0.y() - self.indent) / self.size_sq) + 1)
 
     def open_dialog(self):
-        self.click_pass = True
-        dlg = CustomDialog()
-        dlg.exec()
-
-    def get_pass(self) -> bool:
-        return self.click_pass
+        """creates a dialog box"""
+        point = self.count_points()
+        self.dlg.setStyleSheet(
+                "color: black; font: bold 16px; background-color: white;"
+            )
+        self.dlg.setWindowTitle("Результат игры")
+        self.dlg.setText("Очки игрока: " + str(point[1]) + '\nОчки компьютера: ' + str(point[0]))
+        self.dlg.exec()
 
     def draw_board(self, qp):
+        """
+        creates a board by drawing lines
+        :param qp: instance of QPainter
+        """
         pen = QPen(Qt.black, 3, Qt.SolidLine)
         qp.setPen(pen)
         for i in range(9):
@@ -62,22 +74,44 @@ class MainWindow(QMainWindow):
             qp.drawLine(self.indent + self.size_sq * i, self.indent, self.indent + self.size_sq * i, 9 * self.size_sq)
 
     def draw_stone(self, qp):
+        """
+        draws a stone at the desired point
+        :param qp: instance of QPainter
+        """
         for i in range(1, self.board.size + 1):
             for j in range(1, self.board.size + 1):
-                if self.check_coord(self.size_sq * (i - 1) + self.indent, self.size_sq * (j - 1) + self.indent):
-                    if self.board.board[i][j] == Board.alien:
-                        self.draw_white_stone(qp, self.size_sq * (i - 1) + self.indent,
-                                              self.size_sq * (j - 1) + self.indent)
-                    if self.board.board[i][j] == Board.our:
-                        self.draw_black_stone(qp, self.size_sq * (i - 1) + self.indent,
-                                              self.size_sq * (j - 1) + self.indent)
+                self.set_stone(i, j, qp)
 
-            # верхний левый угол коорд - x - 100, y - 20 разница по 80
+    def set_stone(self, i, j, qp):
+        """
+        checks whether the stone is within the board and determines which stone needs to be drawn
+        :param qp: instance of QPainter
+        :param i: x coordinates
+        :param j: coordinates by y
+        """
+        if self.check_coord(self.size_sq * (i - 1) + self.indent, self.size_sq * (j - 1) + self.indent):
+            if self.board.board[i][j] == Board.alien:
+                self.draw_white_stone(qp, self.size_sq * (i - 1) + self.indent,
+                                      self.size_sq * (j - 1) + self.indent)
+            if self.board.board[i][j] == Board.our:
+                self.draw_black_stone(qp, self.size_sq * (i - 1) + self.indent,
+                                      self.size_sq * (j - 1) + self.indent)
 
     def round_cord(self, point) -> int:
+        """
+        rounds the coordinates received from the mouse click so that the stone is at the intersection of the lines'
+        :param point: x or y coordinates of the player's click
+        :return: rounded coordinates
+        """
         return int(round(point // self.size_sq) * self.size_sq)
 
     def check_coord(self, x, y) -> bool:
+        """
+        checks that the stone does not go beyond the board
+        :param x: coordinates by x
+        :param y: coordinates by y
+        :return: true or false depending on the ability to put a stone
+        """
         if x > 9 * self.size_sq:
             return False
         elif y > 9 * self.size_sq:
@@ -87,69 +121,88 @@ class MainWindow(QMainWindow):
         return True
 
     def draw_white_stone(self, qp, x, y):
+        """
+        a white stone is created
+        :param qp: instance of QPainter
+        :param x: coordinates by x
+        :param y: coordinates by y
+        """
+
         stone = QBrush(Qt.white, Qt.SolidPattern)
         qp.setBrush(stone)
         qp.drawEllipse(x - int(self.size_sq * 0.25), y - int(self.size_sq * 0.25), int(self.size_sq * 0.5),
                        int(self.size_sq * 0.5))
 
     def draw_black_stone(self, qp, x, y):
+        """
+        a black stone is being created
+        :param qp: instance of QPainter
+        :param x: coordinates by x
+        :param y: coordinates by y
+        """
         stone = QBrush(Qt.black, Qt.SolidPattern)
         qp.setBrush(stone)
         qp.drawEllipse(x - int(self.size_sq * 0.25), y - int(self.size_sq * 0.25), int(self.size_sq * 0.5),
                        int(self.size_sq * 0.5))
 
-    def do_person_move(self):
-        self.setEnabled(True)
-        while not self.person_point:
-            continue
-        if self.person_point == "pass":
-            self.count_pass += 1
-        elif self.board.check(self.person_point, Board.alien):
-            self.board.make_move(self.person_point, Board.alien)
-            self.count_pass = 0
-        else:
-            self.person_point = None
-            self.do_person_move()
-        self.person_point = None
-        self.setEnabled(False)
-
     def main(self):
+        """starts a stream with the game"""
         t = threading.Thread(target=self.game)
         t.start()
 
     def game(self):
+        """The main method of the game"""
         stone_type = Board.our
         self.setEnabled(False)
         while True:
             if stone_type == Board.our:
                 if self.flag:
-                    move = IIPlay.play_random_move(self.board)
-                    self.board = move[0]
-                    print(move[1])
-                    for row in self.board.board:
-                        print(row)
-                    if not move[1]:
-                        self.count_pass += 1
-                    else:
-                        self.count_pass = 0
-                    self.update()
-                    self.flag = False
+                    self.do_comp_move()
                 else:
                     continue
+                stone_type = self.board.get_opposite_stone(stone_type)
             if stone_type == Board.alien:
-                if self.click_pass:
-                    self.count_pass += 1
-                else:
-                    self.click_pass = False
-                    self.do_person_move()
-                self.update()
-                self.flag = True
+                self.do_person_move()
+                stone_type = self.board.get_opposite_stone(stone_type)
             if self.count_pass >= 2:
-                self.count_points()
+                self.open_dialog()
                 break
-            stone_type = self.board.get_opposite_stone(stone_type)
+
+    def do_comp_move(self):
+        """creates the computer's progress"""
+        move = IIPlay.play_random_move(self.board)
+        self.board = move[0]
+        if not move[1]:
+            self.count_pass += 1
+        else:
+            self.count_pass = 0
+            self.click_pass = False
+        self.update()
+        self.flag = False
+
+    def do_person_move(self):
+        """processes the player 's move"""
+        self.setEnabled(True)
+        while not self.person_point and not self.click_pass:
+            continue
+        if not self.click_pass:
+            if self.board.check(self.person_point, Board.alien):
+                self.board.make_move(self.person_point, Board.alien)
+                self.count_pass = 0
+                self.click_pass = False
+            else:
+                self.person_point = None
+                self.do_person_move()
+        self.person_point = None
+        self.setEnabled(False)
+        self.update()
+        self.flag = True
 
     def count_points(self) -> list[int]:
+        """
+        computer and player scoring
+        :return: returns a sheet with computer and player points
+        """
         comp_score = 0
         person_score = 0
         for i in range(self.board.size):
@@ -157,21 +210,19 @@ class MainWindow(QMainWindow):
                 point = (i, j)
                 if self.board.get_point_type(point) == Board.empty:
                     survivors = self.board.get_close_neighbors(point)
-                    isCompPoint = True
-                    isPersonPoint = True
+                    is_comp_point = True
+                    is_person_point = True
                     for p in survivors:
                         point_type = self.board.get_point_type(p)
                         if point_type != Board.our and point_type != Board.border:
-                            isCompPoint = False
+                            is_comp_point = False
                             break
-                    for p in survivors:
-                        point_type = self.board.get_point_type(p)
                         if point_type != Board.alien and point_type != Board.border:
-                            isPersonPoint = False
+                            is_person_point = False
                             break
-                    if isCompPoint:
+                    if is_comp_point:
                         comp_score += 1
-                    if isPersonPoint:
+                    if is_person_point:
                         person_score += 1
                 else:
                     if self.board.get_point_type(point) == Board.our:
@@ -179,24 +230,6 @@ class MainWindow(QMainWindow):
                     if self.board.get_point_type(point) == Board.alien:
                         person_score += 1
         return [comp_score, person_score]
-
-
-# qt.drawEllipse(175+80*2, 95+80*2, 50, 50)
-
-
-class CustomDialog(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setFixedSize(300, 100)
-        self.setWindowTitle("Результат")
-        QBtn = QDialogButtonBox.Close
-        self.buttonBox = QDialogButtonBox(QBtn)
-        self.buttonBox.rejected.connect(self.reject)
-        self.layout = QVBoxLayout()
-        message = QLabel("Счет: 0")
-        self.layout.addWidget(message)
-        self.layout.addWidget(self.buttonBox)
-        self.setLayout(self.layout)
 
 
 if __name__ == '__main__':
