64c3314dea04671342e48c26770551087f7fe10c
kli44mar <71692931+kli44mar@users.noreply.github.com>
2022-04-29 13:07:26 +0500

    gui changing the computer's running algorithm

--- IIPlay.py
+++ IIPlay.py
@@ -1,6 +1,5 @@
 import random
-
-from Board import Board
+import Board
 
 
 def get_free_points(board, stone_type):
@@ -14,7 +13,7 @@ def get_free_points(board, stone_type):
         for j in range(1, board.size + 1):
             p = (i, j)
             if board.check_move_correctness(p, stone_type):
-                if board.is_eye_point(p, Board.our) and board.is_real_eye(p):
+                if board.is_eye_point(p, Board.Board.our) and board.is_real_eye(p):
                     continue
                 free_points.append(p)
     return free_points
@@ -25,11 +24,11 @@ def play_random_move(board: Board):
     :param board: the board with current condition of the game
     :return: tuple of new condition the board with set move and the move itself
     """
-    free_points = get_free_points(board, Board.our)
+    free_points = get_free_points(board, Board.Board.our)
     if len(free_points) == 0:
         return board, None
     move = free_points.pop(random.randint(0, len(free_points) - 1))
-    board.make_move(move, Board.our)
+    board.make_move(move, Board.Board.our)
     return board, move
 
 
@@ -39,23 +38,87 @@ def make_comp_move(board: Board):
     :param board: the board with current condition of the game
     :return: returns the board and the coordinates of the stone
     """
+    groups = find_groups_and_count_dame(board)
+    groups = dict(sorted(groups.items(), key=lambda x: x[0]))
+    if not groups:
+        return play_random_move(board)
+    for group in groups.values():
+        if board.is_eye_point(group[0], Board.Board.our) and board.is_real_eye(group[0]):
+            continue
+        point = find_dame(board, group[0])
+        if not point:
+            continue
+        else:
+            if not board.check_move_correctness(point, Board.Board.our):
+                continue
+            board.make_move(point, Board.Board.our)
+            return board, point
+    return play_random_move(board)
+
+
+def find_groups_and_count_dame(board):
+    """
+    :param board: the board with current condition of the game
+    :return: returns a dictionary with the key - the number of free points and the value - the coordinates of the stones
+    """
+    count = 0
+    groups = {}
     for i in range(1, board.size + 1):
         for j in range(1, board.size + 1):
             point = (i, j)
-            neighbors = board.get_close_neighbors(point)
-            if board.get_point_type(point) == Board.alien:
-                no_neighbors = True
-                point_neighbor = None
+            neighbors = find_group(board, point)
+            if neighbors is None:
+                return play_random_move(board)
+            if board.get_point_type(point) == Board.Board.alien:
                 for neighbor in neighbors:
-                    if board.get_point_type(neighbor) == Board.alien:
-                        no_neighbors = False
-                    if (board.get_point_type(neighbor) == Board.empty
-                            and board.check_move_correctness(neighbor, board.our)):
-                        if board.is_eye_point(neighbor, Board.our) and board.is_real_eye(neighbor):
-                            continue
-                        point_neighbor = neighbor
-                        break
-                if no_neighbors and point_neighbor:
-                    board.make_move(point_neighbor, Board.our)
-                    return board, point_neighbor
-    return play_random_move(board)
+                    count = count_dame(board, neighbor)
+                if count not in groups:
+                    groups[count] = neighbors
+    return groups
+
+
+def find_dame(board, point: (int, int)) -> (int, int):
+        """
+        :param point: the point at which the existence of a dame is determined
+        :return: empty point
+        """
+        neighbours = board.get_close_neighbors(point)
+        for neighbour in neighbours:
+            if board.get_point_type(neighbour) == Board.Board.empty:
+                return neighbour
+        return None
+
+
+def find_group(board, point):
+    """
+    :param board: the board with current condition of the game
+    :param point: the point from which the search for the group begins
+    :return: a group of stones
+    """
+    queue = []
+    visited = []
+    if board.get_point_type(point) == Board.Board.alien:
+        queue.append(point)
+        while len(queue) > 0:
+            current = queue.pop()
+            neighbours = board.get_close_neighbors(current)
+            visited.append(current)
+            for neighbour in neighbours:
+                if (board.get_point_type(current) == board.get_point_type(neighbour) and
+                        not Board.is_visited(visited, queue, neighbour)):
+                    queue.append(neighbour)
+    return visited
+
+
+def count_dame(board, point):
+    """
+    :param board: the board with current condition of the game
+    :param point: the point at which the existence of a dame is determined
+    :return: the number of free points next to the stone
+    """
+    count = 0
+    neighbours = board.get_close_neighbors(point)
+    for neighbour in neighbours:
+        if board.get_point_type(neighbour) == Board.Board.empty:
+            count += 1
+    return count
